06d257e2733e63cc044df36a6a4e7879890842e0
Mfreixanet <marcfreixanet@gmail.com>
2023-10-20 00:52:15 +0200

    v1.3

--- validation.js
+++ validation.js
@@ -1,33 +1,30 @@
 const form = document.querySelector(".form");
 const input = document.querySelector(".form-input");
+const emailSpan = document.querySelector(".email");
+const thanksPage = document.querySelector(".thanks-page");
+const content = document.querySelector(".content");
+const dismissButton = document.querySelector(".thanks-button");
 
 form.addEventListener("submit", (e) => {
-  let email = input.value;
-  let emailSpan = document.querySelector(".email");
-  if (!formValidation()) {
-    e.preventDefault();
-  } else {
-    document.querySelector(".content").style.display = "none";
-    document.querySelector(".thanks-page").style.display = "flex";
+  e.preventDefault(); // Previene la presentación del formulario por defecto
+
+  const email = input.value;
+
+  if (formValidation(email)) {
+    content.style.display = "none";
+    thanksPage.style.display = "flex";
     emailSpan.textContent = email;
   }
 });
 
 function formValidation() {
-  // Retorna true si la validación es exitosa, o false si falla
-  // Obtén los valores de los campos del formulario
   const email = document.querySelector(".form-input").value;
-
-  // Inicializa una variable para rastrear errores
   let hasErrors = false;
-
-  // Realiza las validaciones
   if (email.trim() === "") {
     alert("Please, add your email address.");
     hasErrors = true;
   }
 
-  // Valida el formato del correo electrónico utilizando una expresión regular
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!email.match(emailRegex)) {
     const invalidLabel = document.querySelector(".input-label-invalid");
@@ -39,3 +36,10 @@ function formValidation() {
 
   return !hasErrors;
 }
+
+function showInitialContent() {
+  content.style.display = "initial";
+  thanksPage.style.display = "none";
+}
+
+dismissButton.addEventListener("click", showInitialContent);
